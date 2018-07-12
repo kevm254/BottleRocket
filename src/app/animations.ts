5a5a04d6ced180b301f3daf9a9ca7abdf006793e
K M <kevin m>
2018-07-12 09:29:17 -0500

    animation wip

---
+++ src/app/animations.ts
@@ -0,0 +1,25 @@
+import {
+  trigger, animate, transition, style, query
+} from '@angular/animations';
+
+export const fadeAnimation = trigger('fadeAnimation', [
+  // The '* => *' will trigger the animation to change between any two states
+  transition('* => *', [
+    query(
+      ':enter',
+      [style({ opacity: 0 })],
+      { optional: true }
+    ),
+    query(
+      ':leave',
+      // here we apply a style and use the animate function to apply the style over 0.3 seconds
+      [style({ opacity: 1 }), animate('3s', style({ opacity: 0 }))],
+      { optional: true }
+    ),
+    query(
+      ':enter',
+      [style({ opacity: 0 }), animate('3s', style({ opacity: 1 }))],
+      { optional: true }
+    )
+  ])
+]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, transition, group, query, state, style, animate } from '@angular/animations';
+import { fadeAnimation } from './animations';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('routeSlide', [
+      transition('* <=> *', [
+        group([
+          query(':enter', [
+            style({ transform: 'translateX(100%)'}),
+            animate('0.4s ease-in-out', style( { transform: 'translateX(0%)'}))
+          ], { optional: true }),
+          query(':leave', [
+            style({ transform: 'translateX(0%)'}),
+            animate('0.4s ease-in-out', style({ transform: 'translateX(-100%)'}))
+          ], { optional: true }),
+        ])
+      ])
+    ])
+  ]
 
 })
 export class AppComponent {
   title = 'app';
+
+  getState(outletRef: RouterOutlet) {
+    return {
+      value: outletRef.activatedRoute.snapshot.params.index
+    }
+  }
 }
